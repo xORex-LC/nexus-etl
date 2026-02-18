bcc8cbe90327cd18bfefcbed9838cbae055fc421
xORex-LC <denismisurkeev461@gmail.com>
2026-02-18 17:18:22 +0800

    Implement Fernet envelope crypto and ENV key provider

--- connector/infra/secrets/__init__.py
+++ connector/infra/secrets/__init__.py
@@ -3,6 +3,8 @@ from connector.infra.secrets.dict_provider import DictSecretProvider
 from connector.infra.secrets.composite_provider import CompositeSecretProvider
 from connector.infra.secrets.prompt_provider import PromptSecretProvider
 from connector.infra.secrets.file_vault_provider import FileVaultSecretProvider, FileVaultSecretStore
+from connector.infra.secrets.fernet_envelope_cipher import FernetEnvelopeCipher, FERNET_V1
+from connector.infra.secrets.env_key_provider import EnvVaultKeyProvider, parse_master_keyring
 
 __all__ = [
     "NullSecretProvider",
@@ -11,4 +13,8 @@ __all__ = [
     "PromptSecretProvider",
     "FileVaultSecretProvider",
     "FileVaultSecretStore",
+    "EnvVaultKeyProvider",
+    "FERNET_V1",
+    "FernetEnvelopeCipher",
+    "parse_master_keyring",
 ]
